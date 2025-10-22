e619aeaab0f062dfcd8b0c7d6c3e04aa66d7ec82
zbist <zbistolas@gmail.com>
2025-10-22 15:45:52 -0600

    WarCardGame

--- War_Card_Game/src/Deck.java
+++ War_Card_Game/src/Deck.java
@@ -19,47 +19,115 @@ public class Deck {
 		}
 
 	}
-	
+
 	public void selectionSort() {
-		for (int i = 0; i < cards.length -1; i++) {
+		for (int i = 0; i < cards.length - 1; i++) {
 			int bigindex = 0;
-			for (int j = 1; j < cards.length -i; j++) {
-				if(cards[j].compareTo(cards[bigindex]) > 0)
-				{
-					bigindex = j;
-					
-				}swap(bigindex, cards.length - 1 -i);
-			}
-		}
-		
-	}
-	
-	public void insertionSort() {
-		for (int i = 1; i < cards.length; i++) {
-			for (int j = i; (j > 0) && (cards[j].compareTo(cards[j-1]) < 0); j--) {
-				swap(j,j-1);
-				
-			}
-			
-		}
-		
-	}
-	
-	public void bubbleSort()	{
-		for (int i = 0; i < cards.length; i++) {
 			for (int j = 1; j < cards.length - i; j++) {
-				if(cards[j-1].compareTo(cards[j]) > 0)
-				{
-					swap(j-1,j);
+				if (cards[j].compareTo(cards[bigindex]) > 0) {
+					bigindex = j;
+
 				}
+				swap(bigindex, cards.length - 1 - i);
 			}
-			
 		}
 
-		
-			
 	}
-	
+
+	public void insertionSort() {
+		for (int i = 1; i < cards.length; i++) {
+			for (int j = i; (j > 0) && (cards[j].compareTo(cards[j - 1]) < 0); j--) {
+				swap(j, j - 1);
+
+			}
+
+		}
+
+	}
+
+	public void bubbleSort() {
+		for (int i = 0; i < cards.length; i++) {
+			for (int j = 1; j < cards.length - i; j++) {
+				if (cards[j - 1].compareTo(cards[j]) > 0) {
+					swap(j - 1, j);
+				}
+			}
+
+		}
+
+	}
+
+	private int indexLowest(int low, int high) {
+		int minIndex = low;
+		for (int i = low + 1; i <= high; i++) {
+			if (cards[i].compareTo(cards[minIndex]) < 0) {
+				minIndex = i;
+			}
+		}
+		return minIndex;
+	}
+
+	public Deck subdeck(int low, int high) {
+		Deck sub = new Deck(high - low + 1);
+		for (int i = 0; i < sub.cards.length; i++) {
+			sub.cards[i] = this.cards[low + i];
+		}
+		return sub;
+	}
+
+	private static Deck merge(Deck d1, Deck d2) {
+		int n1 = d1.cards.length;
+		int n2 = d2.cards.length;
+
+		Deck d3 = new Deck(n1 + n2);
+
+		int i = 0;
+		int j = 0;
+
+		for (int k = 0; k < d3.cards.length; k++) {
+
+			if (i >= n1) {
+				d3.cards[k] = d2.cards[j];
+				j++;
+			}
+
+			else if (j >= n2) {
+				d3.cards[k] = d1.cards[i];
+				i++;
+			}
+
+			else if (d1.cards[i].compareTo(d2.cards[j]) <= 0) {
+
+				d3.cards[k] = d1.cards[i];
+				i++;
+			} else {
+
+				d3.cards[k] = d2.cards[j];
+				j++;
+			}
+		}
+
+		return d3;
+	}
+
+	public Deck mergeSort() {
+		if (cards.length <= 1) {
+			return this;
+		}
+
+		int mid = (cards.length - 1) / 2;
+
+		Deck left = subdeck(0, mid);
+		Deck right = subdeck(mid + 1, cards.length - 1);
+
+		Deck sortedLeft = left.mergeSort();
+		Deck sortedRight = right.mergeSort();
+
+		Deck merged = merge(sortedLeft, sortedRight);
+
+		return merged;
+	}
+
 	public Card[] getCards() {
 		return cards;
 	}
